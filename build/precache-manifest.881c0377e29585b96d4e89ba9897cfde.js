5954e0617419e588aa26144390666c03fd684974
panjun85553 <j.pan11@newcastle.ac.uk>
2020-05-21 18:04:57 +0800

    first commit

---
+++ build/precache-manifest.881c0377e29585b96d4e89ba9897cfde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1ab572de45f6ec5e83b73886d25fb2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "110c79ef3167aafe5776",
+    "url": "./static/css/2.dafc00e9.chunk.css"
+  },
+  {
+    "revision": "be64520598b3f22f0417",
+    "url": "./static/css/main.ac5d8ae5.chunk.css"
+  },
+  {
+    "revision": "110c79ef3167aafe5776",
+    "url": "./static/js/2.e0743c38.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.e0743c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be64520598b3f22f0417",
+    "url": "./static/js/main.8eb6376b.chunk.js"
+  },
+  {
+    "revision": "fe492bec4e044aabfe13",
+    "url": "./static/js/runtime-main.4f92633c.js"
+  },
+  {
+    "revision": "1e5fb6838887a0990aed9523379f2aba",
+    "url": "./static/media/logo.1e5fb683.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c2f3ed5d09315b8e592c696c61cf44c.js"
+  "./precache-manifest.881c0377e29585b96d4e89ba9897cfde.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
