84d844596c974c7e9dafa556d4a33299ac86e5b5
panjun85553 <j.pan11@newcastle.ac.uk>
2020-05-21 17:40:37 +0800

    first commit

---
+++ build/precache-manifest.7c2f3ed5d09315b8e592c696c61cf44c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4771532ce71c835824f968fb28b11d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "110c79ef3167aafe5776",
+    "url": "/static/css/2.dafc00e9.chunk.css"
+  },
+  {
+    "revision": "be64520598b3f22f0417",
+    "url": "/static/css/main.ac5d8ae5.chunk.css"
+  },
+  {
+    "revision": "110c79ef3167aafe5776",
+    "url": "/static/js/2.e0743c38.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e0743c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be64520598b3f22f0417",
+    "url": "/static/js/main.8eb6376b.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "1e5fb6838887a0990aed9523379f2aba",
+    "url": "/static/media/logo.1e5fb683.png"
+  }
+]);
